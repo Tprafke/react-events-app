bb88bf7318a8f9be92deaecb1fc7cc7958cf6f5d
toby <rtprafke96@gmail.com>
2022-11-05 13:20:53 -0400

    API - Joining events as attendees, and editing events as host

--- Domain/Event.cs
+++ Domain/Event.cs
@@ -9,6 +9,7 @@ namespace Domain
         public string Category { get; set; }
         public string City { get; set; }
         public string Venue { get; set; }
+        public bool IsCancelled { get; set; }
         public ICollection<EventAttendee> Attendees { get; set; } = new List<EventAttendee>();
 
     }
