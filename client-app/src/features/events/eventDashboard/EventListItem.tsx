e42690f90f4cb7071c74aee5c820ed7bbac17a42
toby <rtprafke96@gmail.com>
2022-11-11 16:31:39 -0500

    client - user profile page

--- client-app/src/features/events/eventDashboard/EventListItem.tsx
+++ client-app/src/features/events/eventDashboard/EventListItem.tsx
@@ -26,14 +26,17 @@ export default function EventListItem({ event }: Props) {
               style={{ marginBottom: 5 }}
               size='tiny'
               circular
-              src='/assets/Images/user.png'
+              src={event.host?.image || "/assets/Images/user.png"}
             />
             <Item.Content>
               <Item.Header as={Link} to={`/events/${event.id}`}>
                 {event.title}
               </Item.Header>
               <Item.Description>
-                Hosted by {event.host?.displayName}
+                Hosted by{" "}
+                <Link to={`/profiles/${event.hostUsername}`}>
+                  {event.host?.displayName}
+                </Link>
               </Item.Description>
               {event.isHost && (
                 <Item.Description>
