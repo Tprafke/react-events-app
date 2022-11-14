b411c2e9ad93f319120bfa6955567362461cc6ae
toby <rtprafke96@gmail.com>
2022-11-14 14:43:49 -0500

    client - completed photo upload functionality

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -5,7 +5,7 @@ import { toast } from 'react-toastify';
 import { Event, EventFormValues } from '../models/event';
 import { store } from '../stores/store';
 import { User } from '../models/user';
-import { Profile } from '../models/profile';
+import { Photo, Profile } from '../models/profile';
 
 const sleep = (delay: number) => {
     return new Promise((resolve) => {
@@ -85,7 +85,16 @@ const Account = {
 }
 
 const Profiles = {
-    get: (username: string) => requests.get<Profile>(`/profiles/${username}`)
+    get: (username: string) => requests.get<Profile>(`/profiles/${username}`),
+    uploadPhoto: (file: Blob) => {
+        let formData = new FormData();
+        formData.append('File', file);
+        return axios.post<Photo>('photos', formData, {
+            headers: {'Content-Type': 'multipart/form-data'}
+        })
+    },
+    setMainPhoto: (id: string) => requests.post(`/photos/${id}/setMain`, {}),
+    deletePhoto: (id: string) => requests.del(`/photos/${id}`)
 }
 
 const agent = {
